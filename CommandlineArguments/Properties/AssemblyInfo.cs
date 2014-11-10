c9c7914eb7929a253a8f2dcf55db49069748459a
EliLivshitz <eli.livshitz@gmail.com>
2014-11-10 16:33:27 +0200

    renaming and setting nuget package builder on release

--- CommandlineArguments/Properties/AssemblyInfo.cs
+++ CommandlineArguments/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CommandlineArguments")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Liv.CommandlineArguments")]
+[assembly: AssemblyDescription("C# CommandlineArguments is easy library to implement options/switches support in your console app")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CommandlineArguments")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Livshitz")]
+[assembly: AssemblyProduct("Liv.CommandlineArguments")]
+[assembly: AssemblyCopyright("Copyright © Eli Livshitz 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersionAttribute("1.0.0.1")]
